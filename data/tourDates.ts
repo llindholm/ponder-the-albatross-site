2dcf8cde7f5958185c06c4793bb571972ccc940c
Luke Lindholm <lukelindholm@MacBookPro.lan>
2026-07-28 15:56:44 -0400

    added tour dates

--- data/tourDates.ts
+++ data/tourDates.ts
@@ -19,22 +19,40 @@ export const tourDates = [
     ticketUrl: "#",
   },
   {
+    date: "September 1, 2026",
+    city: "Conway, AR",
+    venue: "The Bear's Den",
+    ticketUrl: "https://www.bearsdenpizza.net/5402-2",
+  },
+  {
+    date: "September 2, 2026",
+    city: "Tahlequah, OK",
+    venue: "Ned's",
+    ticketUrl: "https://www.facebook.com/NedsTahlequah/",
+  },
+  {
     date: "September 3, 2026",
     city: "Eureka Springs, AR",
-    venue: "The Barefoot Ball (in support of Chucky Waggs)",
+    venue: "The Barefoot Ball @ Ozark Folk Festival",
     ticketUrl: "https://www.facebook.com/events/36-s-main-street-eureka-springs-ar-united-states-arkansas-72632/barefoot-ball/4611801655721807/",
   },
   {
     date: "September 4, 2026",
     city: "Jackson, TN",
     venue: "Downtown Tavern",
-    ticketUrl: "#",
+    ticketUrl: "https://www.thedowntowntavern.com/event-details/ponder-the-albatross",
   },
   {
     date: "September 5, 2026",
     city: "Madison, TN",
     venue: "Dee's Country Cocktail Lounge",
-    ticketUrl: "#",
+    ticketUrl: "https://deeslounge.com/",
+  },
+  {
+    date: "October 8, 2026",
+    city: "Eureka Springs, AR",
+    venue: "Hillberry Music Festival",
+    ticketUrl: "https://hillberryfestival.com/",
   },
   {
     date: "November 6-8, 2026",
